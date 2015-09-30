894784a2367cf40167b0d44fbaab812caced3635
khaled Mohamed <khaledmohamedp@gmail.com>
2015-10-01 00:33:32 +0300

    chore(build): Added Semantic-release, Istanbul, commitizen, and Travis to be part of the build

--- spec/BinaryHeapspec.js
+++ spec/BinaryHeapspec.js
@@ -3,20 +3,40 @@ var BinaryHeap = require("../src/BinaryHeap.js");
 describe("BinaryHeap test suit", function(){
     var charList = new BinaryHeap({ order: 'descending', comparable: function(x){return x.age;} });
 
-    it("should returns min value", function(){
+    it("should returns minimum object ", function(){
         charList.insert({'name': 'John', 'age': 25})
         charList.insert({'name': 'Mike', 'age': 21})
-        charList.insert({'name': 'Aisha', 'age': 33})
-        charList.insert({'name': 'Sarah', 'age': 20})
-        charList.insert({'name': 'Tom', 'age': 100})
         charList.insert({'name': 'Alan', 'age': 18})
 
         var removedItem = charList.remove();
 
-        expect(removedItem.name).toEqual("Alan");
-    })
+        expect(removedItem).toEqual({'name': 'Alan', 'age': 18});
+    });
+
+    it("should returns maximum object", function(){
+        
+        var numberList = new BinaryHeap({ data: [2,44,90,20,3,444,2203], order: 'asc' });
+        expect(numberList.remove()).toEqual(2203);
+    });
 
     it("Should returns an number", function(){
-        expect(typeof charList.size()).toEqual("number");
-    })
+        expect(typeof charList.size()).toBe("number");
+    });
+
+    it("Should peak into the removeable elm", function(){
+        var letterList = new BinaryHeap({ data: ["A","B","W","T","H","Y","N"], order: 'asc' });
+        expect(letterList.peak()).toEqual("Y")
+    });
+
+    it("when removing form an empty set", function() {
+        var emptySet = new BinaryHeap();
+        expect(emptySet.remove()).toBeNull();
+    });
+
+    it("should print the list", function(){
+        var letterList = new BinaryHeap({ data: ["A","B","W","T"], order: 'asc' });
+        var tree = letterList.print()
+        
+        expect(tree).toContain("W: T B")
+    });
 });
--- src/BinaryHeap.js
+++ src/BinaryHeap.js
@@ -7,23 +7,30 @@
  *        *------------^
  *        (2 * i) + 1
  */
+"use strict"; 
 
 function BinaryHeap(options) {
-    this.options = options || {};
+    this.options = options || {
+        data: null,
+        comparable: null,
+        order: null
+    };
 
-    this.content = ['ø'];
+    this.content = ["ø"];
 
     // setting options
-    this.options.comparable = this.options.comparable || function(x){ return x; };
-    this.options.order = this.options.order || 'asc';
+    this.options.comparable = this.options.comparable || function(x) {
+        return x;
+    };
+    this.options.order = this.options.order || "asc";
 
     // protect setting not to be change ES6 const
     Object.freeze(this.options);
 
     // if preData is given, insert into the BinaryHeap
-    if(Array.isArray(this.options.data)){
-        for(var i = 0; i < data.length; i++)
-            this.insert(data[i]);
+    if (Array.isArray(this.options.data)) {
+        for (var i = 0; i < this.options.data.length; i++)
+            this.insert(this.options.data[i]);
     }
 }
 
@@ -35,13 +42,13 @@ BinaryHeap.prototype = {
     },
     remove: function() {
         var size = this.size();
-        if(size === 0) return null;
+        if (size === 0){return null;}
         this.exchange(1, size); // 1 = root element
         var elm = this.content.pop();
         this.sinkDown(1);
         return elm;
     },
-    peak: function(){
+    peak: function() {
         return this.content[1];
     },
     swimUp: function(index) {
@@ -49,7 +56,7 @@ BinaryHeap.prototype = {
         while (index > 1 && this.compare(parent, index)) {
             this.exchange(parent, index);
             index = Math.floor(index / 2);
-            parent = Math.floor(index / 2)
+            parent = Math.floor(index / 2);
         }
     },
     sinkDown: function(k) {
@@ -63,12 +70,12 @@ BinaryHeap.prototype = {
             k = j;
         }
     },
-    compare: function(a, b){
+    compare: function(a, b) {
         var elmA = this.options.comparable(this.content[a]);
         var elmB = this.options.comparable(this.content[b]);
-        if(this.options.order === 'asc'){
+        if (this.options.order === "asc") {
             return this.less(elmA, elmB);
-        }else{
+        } else {
             return this.bigger(elmA, elmB);
         }
     },
@@ -79,7 +86,7 @@ BinaryHeap.prototype = {
         return (a > b);
     },
     exchange: function(a, b) {
-        var temp = this.content[a]
+        var temp = this.content[a];
         this.content[a] = this.content[b];
         this.content[b] = temp;
     },
@@ -87,15 +94,18 @@ BinaryHeap.prototype = {
         return this.content.length - 1;
     },
     print: function() {
+        var treeStruct = ""; 
         for (var i = 1; i < this.content.length; i++) {
             var childL = (2 * i);
             var childR = (2 * i) + 1;
-            if (this.content[childL] || this.content[childR])
-                console.log(this.content[i], this.content[childL], this.content[childR])
+            if (this.content[childL] || this.content[childR]){
+                treeStruct += this.content[i] +": "+ this.content[childL] +" "+ this.content[childR] + "\n";
+            }
         }
+        return treeStruct; 
     }
 
 };
 
 
-module.exports =  BinaryHeap;
+module.exports = BinaryHeap;
