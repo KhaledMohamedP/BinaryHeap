4173a0db1fe99cce9170bafd944b05601cf89e26
khaled Mohamed <khaledmohamedp@gmail.com>
2015-10-01 00:58:33 +0300

    chore(build): Removed eslint form the travis build

--- spec/BinaryHeapspec.js
+++ spec/BinaryHeapspec.js
@@ -1,9 +1,9 @@
 var BinaryHeap = require("../src/BinaryHeap.js");
 
-describe("BinaryHeap test suit", function(){
+describe("BinaryHeap", function(){
     var charList = new BinaryHeap({ order: 'descending', comparable: function(x){return x.age;} });
 
-    it("should returns minimum object ", function(){
+    it("should returns Youngest person ", function(){
         charList.insert({'name': 'John', 'age': 25})
         charList.insert({'name': 'Mike', 'age': 21})
         charList.insert({'name': 'Alan', 'age': 18})
@@ -13,30 +13,30 @@ describe("BinaryHeap test suit", function(){
         expect(removedItem).toEqual({'name': 'Alan', 'age': 18});
     });
 
-    it("should returns maximum object", function(){
+    it("should returns largest number", function(){
         
         var numberList = new BinaryHeap({ data: [2,44,90,20,3,444,2203], order: 'asc' });
         expect(numberList.remove()).toEqual(2203);
     });
 
-    it("Should returns an number", function(){
+    it("Should return a number", function(){
         expect(typeof charList.size()).toBe("number");
     });
 
-    it("Should peak into the removeable elm", function(){
+    it("Should peak into the removable/highest letter", function(){
         var letterList = new BinaryHeap({ data: ["A","B","W","T","H","Y","N"], order: 'asc' });
         expect(letterList.peak()).toEqual("Y")
     });
 
-    it("when removing form an empty set", function() {
+    it("when removing from an empty set it should return null", function() {
         var emptySet = new BinaryHeap();
         expect(emptySet.remove()).toBeNull();
     });
 
-    it("should print the list", function(){
+    it("should print the list in a tree structure", function(){
         var letterList = new BinaryHeap({ data: ["A","B","W","T"], order: 'asc' });
         var tree = letterList.print()
-        
+
         expect(tree).toContain("W: T B")
     });
 });
